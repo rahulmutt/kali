aceaea8480ed7d551149db6aaf2a0a07b5f9db87
Rahul Muttineni <rahulmutt@gmail.com>
2026-02-14 18:54:30 +0100

    Setup syn parsing

--- crates/kali/src/main.rs
+++ crates/kali/src/main.rs
@@ -1,7 +1,9 @@
 mod cli;
+mod parse;
 
 use anyhow::Result;
 use cli::{Cli, Commands, Parser};
+use parse::parse_and_debug;
 use std::io::Read;
 
 fn main() -> Result<()> {
@@ -11,9 +13,7 @@ fn main() -> Result<()> {
         Commands::Run { mut input } => {
             let mut s = String::new();
             input.read_to_string(&mut s)?;
-            println!("{s}");
+            parse_and_debug(&s)
         }
     }
-
-    Ok(())
 }
