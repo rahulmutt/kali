84ee4d3513acad62d1d613a2c25d97541691cfa1
Rahul Muttineni <rahulmutt@gmail.com>
2026-02-14 18:23:48 +0100

    Parse input

--- crates/kali/src/main.rs
+++ crates/kali/src/main.rs
@@ -1,13 +1,19 @@
 mod cli;
 
+use anyhow::Result;
 use cli::{Cli, Commands, Parser};
+use std::io::Read;
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Run { input } => {
-            println!("Running with input: {:?}", input);
+        Commands::Run { mut input } => {
+            let mut s = String::new();
+            input.read_to_string(&mut s)?;
+            println!("{s}");
         }
     }
+
+    Ok(())
 }
