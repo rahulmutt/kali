bbec15b614bd666ab2d08b3cf4c1bbaafae4526d
Rahul Muttineni <rahulmutt@gmail.com>
2026-05-15 22:07:40 +0000

    test: add process.kill freeze alias coverage [phase 21]

--- crates/kali_codegen/src/tests.rs
+++ crates/kali_codegen/src/tests.rs
@@ -4131,6 +4131,7 @@ fn process_kill_zero_probe_lowers_through_transparent_wrappers_without_process_e
         "process.kill(+0);",
         "Object.freeze(process.kill)(0);",
         "Object.freeze(globalThis.process.kill)(0);",
+        "Object.freeze(globalThis.process[\"kill\"])(0);",
         "Object.freeze(globalThis[\"process\"][\"kill\"])(0);",
     ] {
         let program = parse_and_lower_lir(source);
