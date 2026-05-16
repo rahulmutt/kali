08b4bb81f5d6988792acbde857d7cc6e2acd89be
Rahul Muttineni <rahulmutt@gmail.com>
2026-05-16 23:20:40 +0000

    test: cover parenthesized process.kill freeze aliases [phase 21]

--- crates/kali_codegen/src/tests.rs
+++ crates/kali_codegen/src/tests.rs
@@ -4130,6 +4130,8 @@ fn process_kill_zero_probe_lowers_through_transparent_wrappers_without_process_e
         "process.kill((0));",
         "process.kill(+0);",
         "Object.freeze(process.kill)(0);",
+        "Object.freeze((process.kill))(0);",
+        "Object.freeze((process.kill))(+0);",
         "Object.freeze(globalThis.process.kill)(0); Object.freeze(globalThis.process.kill)(+0);",
         "Object.freeze(globalThis.process[\"kill\"])(0);",
         "Object.freeze(globalThis[\"process\"].kill)(0);",
