109c1106be801c697ca0abb529b11248b797b339
Rahul Muttineni <rahulmutt@gmail.com>
2026-05-21 10:03:34 +0000

    test: align Promise.allSettled runtime smoke inventory

--- crates/kali_cli/tests/runtime_smoke.rs
+++ crates/kali_cli/tests/runtime_smoke.rs
@@ -40353,7 +40353,7 @@ fn assert_non_literal_dynamic_import_rejection_json(errors: &[Value]) {
     );
 }
 
-fn promise_all_settled_source_variants() -> [&'static str; 15] {
+fn promise_all_settled_source_variants() -> [&'static str; 20] {
     [
         "console.log(Promise.allSettled([1, 2]));\n",
         "console.log(Promise[\"allSettled\"]([1, 2]));\n",
@@ -40365,11 +40365,16 @@ fn promise_all_settled_source_variants() -> [&'static str; 15] {
         "console.log(globalThis['Promise'].allSettled([1, 2]));\n",
         "console.log(globalThis[\"Promise\"][\"allSettled\"]([1, 2]));\n",
         "console.log(globalThis['Promise']['allSettled']([1, 2]));\n",
-        "console.log(Object.freeze(Promise.allSettled)([1, 2]));\n",
-        "console.log(Object.freeze((Promise.allSettled))([1, 2]));\n",
         "console.log(Object.freeze(Promise[\"allSettled\"])([1, 2]));\n",
         "console.log(Object.freeze((Promise[\"allSettled\"]))([1, 2]));\n",
-        "console.log(Object.freeze(globalThis[\"Promise\"][\"allSettled\"])([1, 2]));\n",
+        "console.log(Object.freeze(Promise.allSettled)([1, 2]));\n",
+        "console.log(Object.freeze((Promise.allSettled))([1, 2]));\n",
+        "console.log(Object.freeze(globalThis.Promise.allSettled)([1, 2]));\n",
+        "console.log(Object.freeze((globalThis.Promise.allSettled))([1, 2]));\n",
+        "console.log(Object.freeze(globalThis.Promise[\"allSettled\"])([1, 2]));\n",
+        "console.log(Object.freeze((globalThis.Promise[\"allSettled\"]))([1, 2]));\n",
+        "console.log(Object.freeze(globalThis[\"Promise\"].allSettled)([1, 2]));\n",
+        "console.log(Object.freeze((globalThis[\"Promise\"].allSettled))([1, 2]));\n",
     ]
 }
 
